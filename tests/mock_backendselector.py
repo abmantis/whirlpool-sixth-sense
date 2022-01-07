f3105efb413bf30d0b56c050c533b71bad6e4425
abmantis <amfcalt@gmail.com>
2022-01-07 18:59:14 +0000

    Add missing mock

---
+++ tests/mock_backendselector.py
@@ -0,0 +1,16 @@
+class BackendSelectorMock:
+    @property
+    def brand(self):
+        return "dummy_brand"
+
+    @property
+    def base_url(self):
+        return "dummy_base_url"
+
+    @property
+    def client_id(self):
+        return "dummy_client_id"
+
+    @property
+    def client_secret(self):
+        return "dummy_client_secret"
