53d5f4d5bbfd9218809970b4fb514cde95bfea69
abmantis <amfcalt@gmail.com>
2022-01-07 01:55:54 +0000

    Rename whirlpool_ac.py to cli_ac.py

--- cli_ac.py
+++ cli_ac.py
@@ -8,6 +8,9 @@ from whirlpool.auth import Auth
 
 logging.basicConfig(format="%(asctime)s [%(name)s %(levelname)s]: %(message)s")
 logger = logging.getLogger("whirlpool")
+logger.setLevel(logging.DEBUG)
+
+logger = logging.getLogger("whirlpool.eventsocket")
 logger.setLevel(logging.INFO)
 
 parser = argparse.ArgumentParser()
