5505e8841643c167eb046babe749c54620411aae
Shane Connelly <eskibars@macbook.shane.home.connelly.casa>
2022-01-18 13:54:07 -0800

    Add control lock, door, and display brightness

--- cli_oven_menu.py
+++ cli_oven_menu.py
@@ -1,12 +1,12 @@
 import aioconsole
-
-from whirlpool.oven import Oven
+from whirlpool.oven import Oven, Cavity
 
 async def show_oven_menu(backend_selector, auth, said):
     def print_menu():
         print("\n")
         print(30 * "-", "MENU", 30 * "-")
         print("u. Update status from server")
+        print("l. Control lock toggle")
         print("p. Print status")
         print("v. Print raw status")
         print("c. Custom command")
@@ -15,6 +15,11 @@ async def show_oven_menu(backend_selector, auth, said):
 
     def print_status(ov: Oven):
         print("online: " + str(ov.get_online()))
+        print("meat probe: " + str(ov.get_meat_probe_status()))
+        print("display brightness (%): " + str(ov.get_display_brightness_percent()))
+        print("control lock: " + str(ov.get_control_locked()))
+        print("upper door open: " + str(ov.get_door_opened(Cavity.Upper)))
+        print("lower door open: " + str(ov.get_door_opened(Cavity.Lower)))
 
     def attr_upd():
         print("Attributes updated")
@@ -29,6 +34,8 @@ async def show_oven_menu(backend_selector, auth, said):
 
         if choice == "p":
             print_status(ov)
+        elif choice == "l":
+            await ov.set_control_locked(not ov.get_control_locked())
         elif choice == "u":
             await ov.fetch_data()
             print_status(ov)
--- whirlpool/oven.py
+++ whirlpool/oven.py
@@ -7,6 +7,19 @@ from .appliance import Appliance
 LOGGER = logging.getLogger(__name__)
 
 ATTR_DISPLAY_MEAT_PROBE_STATUS = "OvenUpperCavity_DisplStatusMeatProbeDisplayTemp"
+ATTR_DISPLAY_BRIGHTNESS = "Sys_DisplaySetBrightnessPercent"
+ATTR_CONTROL_LOCK = "Sys_OperationSetControlLock"
+
+ATTR_POSTFIX_DOOR_OPEN_STATUS = "OpStatusDoorOpen"
+
+class Cavity(Enum):
+    Upper = 0
+    Lower = 1
+
+CAVITY_PREFIX_MAP = {
+    Cavity.Upper: "OvenUpperCavity",
+    Cavity.Lower: "OvenLowerCavity"
+}
 
 class Oven(Appliance):
     def __init__(self, backend_selector, auth, said, attr_changed: Callable):
@@ -14,3 +27,19 @@ class Oven(Appliance):
 
     def get_meat_probe_status(self):
         return self.attr_value_to_bool(self.get_attribute(ATTR_DISPLAY_MEAT_PROBE_STATUS))
+
+    def get_door_opened(self, cavity: Cavity = Cavity.Upper):
+        return self.attr_value_to_bool(self.get_attribute(CAVITY_PREFIX_MAP[cavity] + "_" + ATTR_POSTFIX_DOOR_OPEN_STATUS))
+
+    def get_display_brightness_percent(self):
+        return self.get_attribute(ATTR_DISPLAY_BRIGHTNESS)
+
+    async def set_display_brightness_percent(self, pct: int):
+        await self.send_attributes({ATTR_DISPLAY_BRIGHTNESS: str(pct)})
+
+    def get_control_locked(self):
+        return self.attr_value_to_bool(self.get_attribute(ATTR_CONTROL_LOCK))
+
+    async def set_control_locked(self, on: bool):
+        await self.send_attributes({ATTR_CONTROL_LOCK: self.bool_to_attr_value(on)})
+
