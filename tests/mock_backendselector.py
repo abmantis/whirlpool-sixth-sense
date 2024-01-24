9c3aabaed513539073f6b30f7faa189574a6a8d3
Jessica Smith <(none)>
2024-01-24 15:16:37 -0600

    use enums in mockbackend, needed for tests in appliancesmanager

--- tests/mock_backendselector.py
+++ tests/mock_backendselector.py
@@ -1,11 +1,22 @@
+from enum import Enum
+
+
+class DummyBrand(Enum):
+    DUMMY_BRAND = "dummy_brand"
+
+
+class DummyRegion(Enum):
+    DUMMY_REGION = "dummy_region"
+
+
 class BackendSelectorMock:
     @property
     def brand(self):
-        return "dummy_brand"
+        return DummyBrand.DUMMY_BRAND
 
     @property
     def region(self):
-        return "dummy_region"
+        return DummyRegion.DUMMY_REGION
 
     @property
     def base_url(self):
