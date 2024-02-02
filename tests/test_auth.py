aab8d6c5a580cd68c4ce2fa5cd6e0fb81535e2e9
Jessica Smith <(none)>
2024-02-02 09:51:36 -0600

    revert most changes in test_auth, add new test for testing multiple client credentials

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -9,49 +9,28 @@ from whirlpool.auth import Auth
 from .aiohttp import AiohttpClientMocker
 from .mock_backendselector import BackendSelectorMock, BackendSelectorMockMultipleCreds
 
+BACKEND_SELECTOR_MOCK = BackendSelectorMock()
+BACKEND_SELECTOR_MOCK_MULTIPLE_CREDS = BackendSelectorMockMultipleCreds()
+
+AUTH_URL = f"{BACKEND_SELECTOR_MOCK.base_url}/oauth/token"
+AUTH_DATA = {
+    "client_id": BACKEND_SELECTOR_MOCK.client_credentials[0]["client_id"],
+    "client_secret": BACKEND_SELECTOR_MOCK.client_credentials[0]["client_secret"],
+    "grant_type": "password",
+    "username": "email",
+    "password": "secretpass",
+}
 AUTH_HEADERS = {
     "Content-Type": "application/x-www-form-urlencoded",
 }
 
-BACKEND_SELECTORS = [BackendSelectorMockMultipleCreds(), BackendSelectorMock()]
 
 pytestmark = pytest.mark.asyncio
 
 
-@pytest.mark.parametrize("refresh_token", [None, "refresh_token_123"])
-@pytest.mark.parametrize("backend_selector", BACKEND_SELECTORS)
-async def test_get_auth_body(
-    backend_selector, refresh_token, http_client_mock: AiohttpClientMocker
-):
+async def test_auth_success(http_client_mock: AiohttpClientMocker):
     http_client_mock.create_session(asyncio.get_event_loop())
-    auth = Auth(
-        backend_selector=backend_selector,
-        username="email",
-        password="secretpass",
-        session=http_client_mock.session,
-    )
-    for cred in backend_selector.client_credentials:
-        auth_data = auth._get_auth_body(refresh_token, cred)
-
-        assert auth_data["client_id"] == cred["client_id"]
-        assert auth_data["client_secret"] == cred["client_secret"]
-
-        if not refresh_token:
-            assert auth_data["grant_type"] == "password"
-            assert auth_data["username"] == "email"
-            assert auth_data["password"] == "secretpass"
-
-        if refresh_token:
-            assert auth_data["grant_type"] == "refresh_token"
-            assert auth_data["refresh_token"] == refresh_token
-
-    await http_client_mock.close_session()
-
-
-@pytest.mark.parametrize("backend_selector", BACKEND_SELECTORS)
-async def test_auth_success(backend_selector, http_client_mock: AiohttpClientMocker):
-    http_client_mock.create_session(asyncio.get_event_loop())
-    auth = Auth(backend_selector, "email", "secretpass", http_client_mock.session)
+    auth = Auth(BACKEND_SELECTOR_MOCK, "email", "secretpass", http_client_mock.session)
 
     mock_resp_data = {
         "access_token": "acess_token_123",
@@ -63,37 +42,69 @@ async def test_auth_success(backend_selector, http_client_mock: AiohttpClientMoc
         "SAID": ["SAID1", "SAID2"],
         "jti": "?????",
     }
-    http_client_mock.post(backend_selector.auth_url, json=mock_resp_data)
+    http_client_mock.post(AUTH_URL, json=mock_resp_data)
 
     await auth.do_auth(store=False)
     assert auth.is_access_token_valid()
     assert auth.get_said_list() == ["SAID1", "SAID2"]
+    assert len(http_client_mock.mock_calls) == 1
     assert http_client_mock.mock_calls[-1][0] == "POST"
-    assert http_client_mock.mock_calls[-1][1] == URL(backend_selector.auth_url)
+    assert http_client_mock.mock_calls[-1][1] == URL(AUTH_URL)
+    assert http_client_mock.mock_calls[-1][2] == AUTH_DATA
     assert http_client_mock.mock_calls[-1][3] == AUTH_HEADERS
     await http_client_mock.close_session()
 
 
-@pytest.mark.parametrize("backend_selector", BACKEND_SELECTORS)
-async def test_auth_bad_credentials(
-    backend_selector, http_client_mock: AiohttpClientMocker
-):
+async def test_auth_multiple_client_credentials(http_client_mock: AiohttpClientMocker):
     http_client_mock.create_session(asyncio.get_event_loop())
-    auth = Auth(backend_selector, "email", "secretpass", http_client_mock.session)
+    auth = Auth(BACKEND_SELECTOR_MOCK_MULTIPLE_CREDS, "email", "secretpass", http_client_mock.session)
+    auth_data = AUTH_DATA.copy()
+
+    mock_resp_data = {
+        "access_token": "acess_token_123",
+        "token_type": "bearer",
+        "refresh_token": "refresher_123",
+        "expires_in": 21599,
+        "scope": "trust read write",
+        "accountId": 12345,
+        "SAID": ["SAID1", "SAID2"],
+        "jti": "?????",
+    }
+
+    # create mock for each client credential, all but the last one returning 404 so we can try the next
+    for i in BACKEND_SELECTOR_MOCK_MULTIPLE_CREDS.client_credentials:
+        status = (
+            HTTPStatus.NOT_FOUND if i != len(BACKEND_SELECTOR_MOCK_MULTIPLE_CREDS.client_credentials) else HTTPStatus.OK
+        )
+        http_client_mock.post(AUTH_URL, json=mock_resp_data, status=status)
+
+    await auth.do_auth(store=False)
+
+    # ensure that each client credential is used
+    for i, auth_data in enumerate(BACKEND_SELECTOR_MOCK_MULTIPLE_CREDS.client_credentials):
+        for k, v in auth_data.items():
+            assert http_client_mock.mock_calls[i][2][k] == v
+
+    await http_client_mock.close_session()
+
+
+async def test_auth_bad_credentials(http_client_mock: AiohttpClientMocker):
+    http_client_mock.create_session(asyncio.get_event_loop())
+    auth = Auth(BACKEND_SELECTOR_MOCK, "email", "secretpass", http_client_mock.session)
 
     mock_resp_data = {
         "error": "invalid_request",
         "error_description": "Bad credentials",
         "code": "13000",
     }
-    http_client_mock.post(
-        backend_selector.auth_url, json=mock_resp_data, status=HTTPStatus.BAD_REQUEST
-    )
+    http_client_mock.post(AUTH_URL, json=mock_resp_data, status=HTTPStatus.BAD_REQUEST)
 
     await auth.do_auth(store=False)
-    assert auth.is_access_token_valid() is False
-    assert auth.get_said_list() is None
+    assert auth.is_access_token_valid() == False
+    assert auth.get_said_list() == None
+    assert len(http_client_mock.mock_calls) == 1
     assert http_client_mock.mock_calls[-1][0] == "POST"
-    assert http_client_mock.mock_calls[-1][1] == URL(backend_selector.auth_url)
+    assert http_client_mock.mock_calls[-1][1] == URL(AUTH_URL)
+    assert http_client_mock.mock_calls[-1][2] == AUTH_DATA
     assert http_client_mock.mock_calls[-1][3] == AUTH_HEADERS
     await http_client_mock.close_session()
