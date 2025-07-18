75f7c3c962c39225336394cbd64217ad475e496f
abmantis <amfcalt@gmail.com>
2025-07-18 23:29:20 +0100

    Return bool and raise errors in fridge setters

--- whirlpool/refrigerator.py
+++ whirlpool/refrigerator.py
@@ -23,35 +23,35 @@ class Refrigerator(Appliance):
         raw_temp = self._get_int_attribute(SETTING_TEMP)
         return reversed_temp_map[raw_temp] if raw_temp is not None else None
 
-    async def set_offset_temp(self, temp):
-        if temp in TEMP_MAP.keys():
-            await self.send_attributes({SETTING_TEMP: str(TEMP_MAP[temp])})
-        else:
-            LOGGER.error(
-                f"Invalid temperature: {temp}. Allowed values are {TEMP_MAP.keys()}."
+    async def set_offset_temp(self, temp) -> bool:
+        if temp not in TEMP_MAP.keys():
+            raise ValueError(
+                f"Invalid temperature: {temp}. Allowed values are {TEMP_MAP.keys()}"
             )
+        return await self.send_attributes({SETTING_TEMP: str(TEMP_MAP[temp])})
 
     def get_temp(self) -> int | None:
         return self._get_int_attribute(SETTING_TEMP)
 
-    async def set_temp(self, temp: int):
-        if temp in TEMP_MAP.values():
-            await self.send_attributes({SETTING_TEMP: str(temp)})
-        else:
-            LOGGER.error(
+    async def set_temp(self, temp: int) -> bool:
+        if temp not in TEMP_MAP.values():
+            raise ValueError(
                 f"Invalid temperature: {temp}. Allowed values are {TEMP_MAP.values()}."
             )
+        return await self.send_attributes({SETTING_TEMP: str(temp)})
 
     def get_turbo_mode(self) -> bool | None:
         return self.attr_value_to_bool(self._get_attribute(SETTING_TURBO_MODE))
 
-    async def set_turbo_mode(self, turbo: bool):
-        await self.send_attributes({SETTING_TURBO_MODE: self.bool_to_attr_value(turbo)})
+    async def set_turbo_mode(self, turbo: bool) -> bool:
+        return await self.send_attributes(
+            {SETTING_TURBO_MODE: self.bool_to_attr_value(turbo)}
+        )
 
     def get_display_lock(self) -> bool | None:
         return self.attr_value_to_bool(self._get_attribute(SETTING_DISPLAY_LOCK))
 
-    async def set_display_lock(self, display: bool):
-        await self.send_attributes(
+    async def set_display_lock(self, display: bool) -> bool:
+        return await self.send_attributes(
             {SETTING_DISPLAY_LOCK: self.bool_to_attr_value(display)}
         )
